f89104edfd7e22b62f71f8e4a94ce6a958498be8
Allen R. Lea <72241498+Allen70@users.noreply.github.com>
2021-05-11 14:51:15 -0500

    adding classname for styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,10 @@ import { Link} from 'react-router-dom'
 
 export function Navbar(){
     return(
-        <nav>
-            <Link to='/professional'>Professional</Link> | <Link to='/personal'>Personal</Link> | <Link to='/arcade'>Arcade</Link> 
-        </nav>
+        <div className="navbaar">
+            <nav>
+                <Link to='/professional'>Professional</Link> | <Link to='/personal'>Personal</Link> | <Link to='/arcade'>Arcade</Link> 
+            </nav>
+        </div>
     )
 }
